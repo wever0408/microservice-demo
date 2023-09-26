d2b63bf4785c0095cfeb12c8e3f80baa620422ea
Nguyen Xuan Tam <xuantam0304@gmail.com>
2023-09-26 11:51:21 +0700

    Add dealing with missing events function

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -3,6 +3,18 @@ import axios from 'axios'
 
 const CommentList = ({comments}) => {
     const renderedComments = comments.map(comment => {
+        let content;
+        console.log("comment: ",comment)
+        if(comment.status === 'approved'){
+            content = comment.content;
+        }
+        if(comment.status === 'pending'){
+            content = 'The comment is awaiting moderation';
+        }
+        if(comment.status === 'rejected'){
+            content = 'The comment has been rejected';
+        }
+
         return <li key={comment.id}>{comment.content} </li>
     })
     
--- query/index.js
+++ query/index.js
@@ -1,46 +1,59 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
 
-const app = express()
-app.use(bodyParser.json())
-app.use(cors())
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-    const posts = {}
+const posts = {};
 
-app.get('/posts',(req, res) => {
-    console.log("Posts: ",posts);
+const handleEvent = (type, data) => {
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+    console.log(posts[id]);
+  } else if (type === "CommentCreated") {
+    const { id, content, postId, status } = data;
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
+    console.log(posts[postId]);
+  } else if (type === "CommentUpdated") {
+    const { id, postId, status, content } = data;
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+    comment.content = content;
+  }
+};
 
-    res.send(posts);
-})
+app.get("/posts", (req, res) => {
+  console.log("Posts: ", posts);
 
-app.post('/events', (req, res) => {
-    const {type, data} = req.body;
+  res.send(posts);
+});
 
-    if (type === 'PostCreated'){
-        const {id, title} = data;
-        posts[id] = {id, title, comments: []}
-        console.log(posts[id]);
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
 
+  handleEvent(type, data);
+
+  res.send({});
+});
+
+app.listen(4002, async () => {
+  console.log("Listening on 4002");
+  try {
+    const res = await axios.get("http://localhost:4005/events");
+
+    for (let event of res.data) {
+      console.log("Processing Event: ", event.type);
+      handleEvent(event.type, event.data);
     }
-    else if (type === 'CommentCreated'){
-        const {id, content, postId, status} = data;
-        const post = posts[postId];
-        post.comments.push({id,content,status}); 
-        console.log(posts[postId]);
-    }
-    else if (type === 'CommentUpdated'){
-        const { id, postId, status, content } = data;
-        const post = posts[postId];
-        const comment = post.comments.find((comment) => {
-        return comment.id === id;
-        });
-        comment.status = status;
-        comment.content=content;
-    }
-    res.send({});
-})
-
-app.listen(4002, () => {
-    console.log('Listening on 4002')
-})
+  } catch (error) {
+    console.log(error.message);
+  }
+});
